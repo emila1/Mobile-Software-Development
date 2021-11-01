12c3464d98ae6603fb9cc46203bf6f645b9260c6
Jakob-k <jakaa18@student.sdu.dk>
2021-11-01 14:25:02 +0100

    AuthFlow for Guest login working

--- src/screens/StartScreen/StartScreen.js
+++ src/screens/StartScreen/StartScreen.js
@@ -1,41 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { View, Text, Button, Image } from 'react-native';
 import { getRandomRecipe, searchIngredients } from '../../utils/search'
 import { MealStyles } from '../../styles/global'
+import { AuthContext } from '../../AuthContext/AuthContext';
 
-export default class StartScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
 
-        };
-    }
+export default function StartScreen(props) {
+    const { signInGuest } = useContext(AuthContext)
 
-    render() {
-        console.log(this.state.randomRecipes)
-        return (
-            <View style={MealStyles.container}>
-                <View>
-                    <Image source={require('../../../assets/splash.png')} style={{ resizeMode: "contain", width: 250, height: 250 }} />
-                </View>
-
-                <Text style={MealStyles.red}>Keep your meal up to date......</Text>
-                <Text style={MealStyles.blue}>. . . .</Text>
-
-                <View style={MealStyles.buttonGuest}>
-                    <Button style={MealStyles.buttonStyle} title="Sign in" />
-                </View>
-
-                <View style={MealStyles.buttonGuest}>
-                    <Button style={MealStyles.buttonStyle} title="Continue as guest" onPress={() => this.props.navigation.navigate('MealMe')} />
-                </View>
-
-                <View style={MealStyles.buttonCreat}>
-                    <Button style={MealStyles.buttonStyle} onPress={() => alert("Try guest acount")} title="Create an account" />
-                </View>
-
+    return (
+        <View style={MealStyles.container}>
+            <View>
+                <Image source={require('../../../assets/splash.png')} style={{ resizeMode: "contain", width: 250, height: 250 }} />
             </View>
-        );
-    }
+
+            <Text style={MealStyles.red}>Keep your meal up to date......</Text>
+            <Text style={MealStyles.blue}>. . . .</Text>
+
+            <View style={MealStyles.buttonGuest}>
+                <Button style={MealStyles.buttonStyle} title="Sign in" />
+            </View>
+
+            <View style={MealStyles.buttonGuest}>
+                <Button style={MealStyles.buttonStyle} title="Continue as guest" onPress={() => signInGuest()} />
+            </View>
+
+            <View style={MealStyles.buttonCreat}>
+                <Button style={MealStyles.buttonStyle} onPress={() => alert("Try guest acount")} title="Create an account" />
+            </View>
+
+        </View>
+    );
+
 }
 
