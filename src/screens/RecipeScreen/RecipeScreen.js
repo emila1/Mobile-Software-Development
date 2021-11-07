1254d9d84ef65b218c20403d68bd88d36747dc37
Azzi <ArminAlicajic1324@hotmail.com>
2021-11-07 13:18:00 +0100

    Moved CSS to global.js

--- src/screens/RecipeScreen/RecipeScreen.js
+++ src/screens/RecipeScreen/RecipeScreen.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Text, View, Image, SafeAreaView, TouchableOpacity, TextInput, FlatList, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, TouchableOpacity, FlatList} from 'react-native';
 import { MealStyles } from '../../styles/global';
 import { getRandomRecipe } from '../../utils/search';
-import Recipes from '../../../recipes/recipes.json';
-import RecipeCard from '../../components/recipeCard'
+import RecipeCard from '../../components/recipeCard';
 
-const recipe = Recipes;
-const ind = 55;
 class RecipeScreen extends React.Component {
     constructor(props) {
         super(props);
@@ -17,18 +14,18 @@ class RecipeScreen extends React.Component {
 
     render() {
         return (
-            <SafeAreaView style={test.recipeContainer}>
-                <Text style={test.recipeTitle}>Recipes</Text>
-                <Text style={test.RecipeSubtext}>Select any recipe to display full instructions!</Text>
+            <SafeAreaView style={MealStyles.recipeContainer}>
+                <Text style={MealStyles.recipeTitle}>Recipes</Text>
+                <Text style={MealStyles.RecipeSubtext}>Select any recipe to display full instructions!</Text>
 
-                <FlatList style={test.recipeflatListContainer}
+                <FlatList style={MealStyles.recipeflatListContainer}
                     data={this.state.randomRecipes}
                     keyExtractor={item => this.state.randomRecipes.image_urls} // Less likely to give "must have unique key" warning
                     keyboardDismissMode="on-drag"
                     showsVerticalScrollIndicator={false}
                     renderItem={({item}) => {
                         return (
-                            <TouchableOpacity style={test.recipeTouchContainer}>
+                            <TouchableOpacity style={MealStyles.recipeTouchContainer}>
 
                             <RecipeCard value={item}/>
                 
@@ -40,32 +37,5 @@ class RecipeScreen extends React.Component {
         );
     }
 }
-
-const test = StyleSheet.create({
-    recipeContainer: {
-        flex: 1,
-        backgroundColor: '#C1553E'
-    },
-    recipeflatListContainer: {
-        marginTop: 10,
-    },
-    recipeTouchContainer: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: 10,
-        padding: 10,
-    },
-    recipeTitle: {
-        fontSize: 60,
-        color: '#ffffff',
-        textAlign: 'center',
-        marginTop: 25,
-    },
-    RecipeSubtext: {
-        fontSize: 14,
-        color: '#ffffff',
-        textAlign: 'center',
-    },
-});
 
 export default RecipeScreen;
