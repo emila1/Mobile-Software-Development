154776387363662156dba0df5c9d7e8af64990cc
Azzi <ArminAlicajic1324@hotmail.com>
2021-11-09 16:48:52 +0100

    layout fix

--- src/screens/RecipeInfoScreen/RecipeInfoScreen.js
+++ src/screens/RecipeInfoScreen/RecipeInfoScreen.js
@@ -1,60 +1,61 @@
-import React from 'react';
-import { Text, View, Image, SafeAreaView, StyleSheet, FlatList, List} from 'react-native';
-import { MealStyles } from '../../styles/global';
-import recipes from '../../../recipes/recipes.json'
-import { Link } from '@react-navigation/native';
+import React from "react";
+import {
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  List,
+} from "react-native";
+import { MealStyles } from "../../styles/global";
+import recipes from "../../../recipes/recipes.json";
+import { Link } from "@react-navigation/native";
 
 const recipe = recipes;
-const tabs = ['ingredients', 'instructions'];
+const tabs = ["ingredients", "instructions"];
 
 export default class RecipeInfoScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        };
-    }
+  render() {
+    return (
+      <SafeAreaView style={MealStyles.infoContainer}>
+        <View style={MealStyles.infoImageContainer}>
+          <Image
+            style={MealStyles.infoImage}
+            source={{ uri: recipe[0].image_urls[0] }}
+          />
+        </View>
+        <Text style={MealStyles.infoTextTitle}>{recipe[0].title}</Text>
+        <Text style={MealStyles.infoTextSubtitle}>{recipe[0].head}</Text>
 
-    render() {
-
-        return (
-        <SafeAreaView style={MealStyles.infoContainer}>
-            <View style={MealStyles.infoImageContainer}>
-            <Image 
-                style={MealStyles.infoImage}
-                source={{uri: recipe[0].image_urls[0]}}/>
-            </View>
-            <Text style={MealStyles.infoTextTitle}>{recipe[0].title}</Text>
-            <Text style={MealStyles.infoTextSubtitle}>{recipe[0].head}</Text>
-
-            <View style={MealStyles.infoBoxContainer}>
-                <View style={MealStyles.infoListContainer}>
-                    <View>
-                        <Text style={MealStyles.test}>test</Text>
-                        <View>
-                        <FlatList 
-                        data={recipe}
-                        renderItem={({item}) =>
-                            <Text>{item.ingredients[0]}</Text>
-                        }
-                        />
-                        </View>
-                    </View>
-                    <View>
-                        <Text style={MealStyles.test}>test2</Text>
-                        <View>
-                        <FlatList
-                        data={recipe}
-                        renderItem={({item}) =>
-                            <Text>{item.ingredients[1]}</Text>
-                        }
-                        />
-                        </View>
-                    </View>
-                </View>
-            </View>
-
-        </SafeAreaView>
-        );
-    }
+        <View style={MealStyles.infoBoxContainer}>
+          <View style={MealStyles.infoListContainer}>
+            <Text>test</Text>
+            <Text>test2</Text>
+          </View>
+          <View style={MealStyles.infoListContainer}>
+            <FlatList
+              style={MealStyles.test2}
+              data={recipe}
+              renderItem={({ item }) => (
+                <Text style={MealStyles.test3}>{item.ingredients[1]}</Text>
+              )}
+            />
+            <FlatList
+              style={MealStyles.test4}
+              data={recipe}
+              renderItem={({ item }) => (
+                <Text style={MealStyles.test3}>{item.ingredients[1]}</Text>
+              )}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
