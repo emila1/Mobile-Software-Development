bfabe96e692c841df8f543e23698be1c04bcd33f
Ásbjörn Skarphéðinsson <usska18@student.sdu.dk>
2021-11-12 15:14:27 +0100

    Fix merge errors

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthContext } from './src/AuthContext/AuthContext';
 import ingredients from './src/utils/ingredients.json'
 import IngredientContext from './src/IngredientContext/IngredientContext';
-import { HomeScreen, MyFridgeScreen, RecipeScreen, SettingScreen, StartScreen, RecipeInfoScreen } from './src/screens'
+import { HomeScreen, MyFridgeScreen, RecipeScreen, SettingScreen, StartScreen, SurpriseMeScreen, FavoritesScreen, RecipeInfoScreene } from './src/screens'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from '@expo/vector-icons'
 
@@ -46,11 +46,18 @@ function ShoppingStack({ navigation }) {
     return (
 
         <StackShopping.Navigator initialRouteName="Your Inventory" navigation={navigation} >
-            <StackShopping.Screen name="Your Inventory" component={MyFridgeScreen} options={{ headerShown: false }} />
+            <StackShopping.Screen name="Your Inventory" component={MyFridgeScreen} options={{ headerShown: false}} />
         </StackShopping.Navigator>
     )
 }
 
+function FavoriteStack({ navigation }) {
+    return (
+        <StackFavorite.Navigator navigation={navigation} >
+            <StackFavorite.Screen name="FavoritesScreen" component={FavoritesScreen} options={{ headerShown: false }} />
+        </StackFavorite.Navigator>
+    )
+}
 
 function SettingsStack({ navigation }) {
     return (
@@ -83,6 +90,8 @@ function TabNavigator({ navigation, extraData }) {
                 } else if (route.name === 'Fridge') {
                     iconName = focused ? 'basket' : 'basket-outline'
                 } else if (route.name === 'Recipes') {
+                    iconName = focused ? 'ios-compass' : 'ios-compass-outline'
+                } else if (route.name === 'Favorites') {
                     iconName = focused ? 'restaurant' : 'restaurant-outline'
                 }
 
@@ -96,10 +105,11 @@ function TabNavigator({ navigation, extraData }) {
                 {props => <HomeStack {...props} extraData={extraData} />}
             </Tab.Screen>
             <Tab.Screen name="Fridge" component={ShoppingStack} />
-            <Tab.Screen name="Recipes" component={RecipieStack} />
+            <Tab.Screen name="Favorites" component={FavoriteStack} />
             <Tab.Screen name="Settings" >
                 {props => <SettingsStack {...props} extraData={extraData} />}
             </Tab.Screen>
+            <Tab.Screen name="Recipes" component={RecipieStack} />
         </Tab.Navigator>
     )
 }
@@ -119,7 +129,7 @@ function App() {
             Alert.alert("Navigating to sign in", data)
         },
         signOut: async () => {
-            setUser(null)
+            Alert.alert("Signing out", data)
         },
         signInGuest: async () => {
             setUser('Guest')
@@ -129,25 +139,25 @@ function App() {
 
     return (
         <IngredientContext.Provider value={recievedIngredients}>
-            <AuthContext.Provider value={authContext} >
-                <NavigationContainer>
-                    <MainStack.Navigator initialRouteName="StartScreen" >
-                        {user ? (
-                            <>
-                                <MainStack.Screen name="HomeScreen" options={{ headerShown: false }} >
-                                    {props => <TabNavigator {...props} extraData={user} />}
-                                </MainStack.Screen>
-                            </>
-                        ) : (
-                            <>
-                                <MainStack.Screen name="Start" options={{ headerShown: false }}>
-                                    {props => <AuthStack {...props} />}
-                                </MainStack.Screen>
-                            </>
-                        )}
-                    </MainStack.Navigator>
-                </NavigationContainer>
-            </AuthContext.Provider>
+        <AuthContext.Provider value={authContext} >
+            <NavigationContainer>
+                <MainStack.Navigator initialRouteName="StartScreen" >
+                    {user ? (
+                        <>
+                            <MainStack.Screen name="HomeScreen" options={{ headerShown: false }} >
+                                {props => <TabNavigator {...props} extraData={user} />}
+                            </MainStack.Screen>
+                        </>
+                    ) : (
+                        <>
+                            <MainStack.Screen name="Start" options={{ headerShown: false }}>
+                                {props => <AuthStack {...props} />}
+                            </MainStack.Screen>
+                        </>
+                    )}
+                </MainStack.Navigator>
+            </NavigationContainer>
+        </AuthContext.Provider>
         </IngredientContext.Provider>
     );
 }
