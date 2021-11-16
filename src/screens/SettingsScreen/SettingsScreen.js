036385d6924c7d8f620c515d30b159f63ba1878e
Jakob-k <jakaa18@student.sdu.dk>
2021-11-16 09:55:59 +0100

    Added Switch for Themes

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Button, Modal, Pressable, SafeAreaView, Text, View } from 'react-native';
+import { Button, Modal, Pressable, SafeAreaView, Text, View, Switch } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { AuthContext } from '../../AuthContext/AuthContext.js';
 import { MealStyles } from '../../styles/global.js';
@@ -9,6 +9,8 @@ export default function SettingScreen(props) {
 
     const { signOut, signUp, signIn } = useContext(AuthContext)
     const [modalVisible, setModalVisible] = useState(false)
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
     const user = props.route.params.user
 
     return (
@@ -42,7 +44,7 @@ export default function SettingScreen(props) {
                         <Text style={{ fontSize: 20, fontWeight: 'bold' }} >MealMe app</Text>
                         <Text style={{ padding: 12 }} >Browse through 500 delicious recipes to help you decide your dish</Text>
                         <Text style={{ padding: 12 }}>You can add ingredients to the 'Fridge' tab and MealMe will automatically help filter recipes that includes those ingredients</Text>
-                        <Text style={{ padding: 12 }}>You can pin and unpin your favorite recipes to easily find them again</Text>
+                        <Text style={{ padding: 12 }}>You can pin and unpin your favorite recipes to easily find them again, or clear all pinned recipes</Text>
                         <Text style={{ padding: 12 }}>If you would like to indulge in the attractive and handy features of a premium account for 10$,
                             you can register with an account and start now</Text>
                         <Pressable style={{
@@ -59,32 +61,47 @@ export default function SettingScreen(props) {
             </Modal>
             <Text style={MealStyles.green}>Settings</Text>
             <Text style={{ textDecorationLine: 'underline' }} >Signed in as {user}</Text>
-            <View style={{ alignSelf: 'flex-end', paddingEnd: '5%', paddingTop: '8%' }} >
-                <TouchableOpacity onPress={() => setModalVisible(true)} >
-                    <Entypo name="help-with-circle" size={30} color="black" />
-                </TouchableOpacity>
+            <View style={{ alignContent: 'space-around', width: '80%', flexDirection: 'row', paddingTop: '8%' }} >
+                <View style={{ flex: 1, alignItems: 'baseline' }} >
+                    <Text>Switch Theme</Text>
+                    <Switch
+                        trackColor={{ false: "#81b0ff", true: "#767577" }}
+                        thumbColor={isEnabled ? "#483d8b" : "#f5dd4b"}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={toggleSwitch}
+                        value={isEnabled}
+                    />
+                </View>
+                <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end' }} >
+                    <TouchableOpacity onPress={() => setModalVisible(true)} >
+                        <Entypo name="help-with-circle" size={30} color="black" />
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={{ paddingLeft: '30%', flex: 1, paddingRight: '30%', paddingBottom: '20%', justifyContent: 'center' }} >
-                <Text style={{ paddingTop: '30%', fontSize: 18, textAlign: 'center' }} >Would you like to sign out from {user}?</Text>
-                <TouchableOpacity style={MealStyles.buttonGuest} onPress={() => signOut()} >
-                    <Text style={MealStyles.buttonGuestText} >SIGN OUT</Text>
+            <View style={{ paddingLeft: '30%', flex: 1, paddingRight: '30%', paddingBottom: '20%', justifyContent: 'space-evenly' }} >
+                <Text style={{ paddingTop: '20%', fontSize: 18, textAlign: 'center', paddingBottom: '10%' }} >Remove every pinned recipe here</Text>
+                <TouchableOpacity style={MealStyles.buttonGuest} onPress={() => alert("Clearing every pinned recipe")} >
+                    <Text style={MealStyles.buttonGuestText} >CLEAR RECIPES</Text>
                 </TouchableOpacity>
                 {user == "Guest" ? (
                     <>
-                        <Text style={{ paddingTop: '30%', fontSize: 18, textAlign: 'center' }} >Would you like to create a premium account?</Text>
-                        <TouchableOpacity style={MealStyles.buttonGuest} onPress={() => signUp()} >
-                            <Text style={MealStyles.buttonGuestText} >CREATE ACCOUNT</Text>
-                        </TouchableOpacity>
-
-                        <Text style={{ paddingTop: '30%', fontSize: 18, textAlign: 'center' }}>Or if you already have a premium account:</Text>
+                        <Text style={{ paddingTop: '20%', fontSize: 18, textAlign: 'center' }}>If you already have a premium account:</Text>
                         <TouchableOpacity style={MealStyles.buttonGuest} onPress={() => signIn()} >
                             <Text style={MealStyles.buttonGuestText} >LOG IN</Text>
+                        </TouchableOpacity>
+                        <Text style={{ paddingTop: '20%', fontSize: 18, textAlign: 'center', paddingBottom: '10%' }} >Or would you like to create a premium account?</Text>
+                        <TouchableOpacity style={MealStyles.buttonGuest} onPress={() => signUp()} >
+                            <Text style={MealStyles.buttonGuestText} >CREATE ACCOUNT</Text>
                         </TouchableOpacity>
                     </>
                 ) : (
                     <>
                     </>
                 )}
+                <Text style={{ paddingTop: '20%', fontSize: 18, textAlign: 'center' }} >Would you like to sign out from {user}?</Text>
+                <TouchableOpacity style={MealStyles.buttonGuest} onPress={() => signOut()} >
+                    <Text style={MealStyles.buttonGuestText} >SIGN OUT</Text>
+                </TouchableOpacity>
 
             </View>
         </SafeAreaView >
