d2e954eebd8aac0452edaeefc0f08800643d2b7d
Jakob-k <jakaa18@student.sdu.dk>
2021-11-17 10:01:03 +0100

    Added backbutton

--- src/screens/RecipeInfoScreen/RecipeInfoScreen.js
+++ src/screens/RecipeInfoScreen/RecipeInfoScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, View, Image, SafeAreaView, StyleSheet, FlatList, List, TouchableOpacity} from "react-native";
+import { Text, View, Image, SafeAreaView, StyleSheet, FlatList, List, TouchableOpacity } from "react-native";
 import { MealStyles } from "../../styles/global";
 import recipes from "../../../recipes/recipes.json";
 import { Ionicons } from "@expo/vector-icons";
@@ -15,26 +15,26 @@ export default class RecipeInfoScreen extends React.Component {
     super(props)
     this.getPinData()
     this.state = {
-        isPinned: false,
-        pinnedRecipeIndexes: [],
-        viewedRecipeIndexes: [],
-        index: null,
+      isPinned: false,
+      pinnedRecipeIndexes: [],
+      viewedRecipeIndexes: [],
+      index: null,
     }
-}
+  }
 
-    // This is called at construction. It fetches pinned recipe indexes (if any exist) from local storage
-    getPinData = async () => {
-      try {
-          const items = await AsyncStorage.getItem('pinnedRecipeIndexes')
-          if (items !== null) {
-              this.setState({
-                pinnedRecipeIndexes: JSON.parse(items),
-              });
-          }
-          this.checkIfPinned()
-      } catch (error) {
-          console.log(error.message)
+  // This is called at construction. It fetches pinned recipe indexes (if any exist) from local storage
+  getPinData = async () => {
+    try {
+      const items = await AsyncStorage.getItem('pinnedRecipeIndexes')
+      if (items !== null) {
+        this.setState({
+          pinnedRecipeIndexes: JSON.parse(items),
+        });
       }
+      this.checkIfPinned()
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
   // Checks if this recipe's index is found in the fetched pin indexes and sets pin state accordingly
@@ -51,7 +51,7 @@ export default class RecipeInfoScreen extends React.Component {
   }
 
   // Either removed this recipe index or adds it to the fetched pin indexes
-  handlePin () {
+  handlePin() {
     if (this.state.isPinned == true) {
       const index = this.state.pinnedRecipeIndexes.indexOf(this.state.index)
       this.state.pinnedRecipeIndexes.splice(index, 1)
@@ -64,19 +64,19 @@ export default class RecipeInfoScreen extends React.Component {
   // Fetches the viewed recipe indexes in local storage
   getViewData = async () => {
     try {
-        const items = await AsyncStorage.getItem('viewedRecipeIndexes');
-        if (items !== null) {
-            this.setState({
-              viewedRecipeIndexes: JSON.parse(items),
-            });
-        }
+      const items = await AsyncStorage.getItem('viewedRecipeIndexes');
+      if (items !== null) {
+        this.setState({
+          viewedRecipeIndexes: JSON.parse(items),
+        });
+      }
     } catch (error) {
-        console.log(error.message)
+      console.log(error.message)
     }
     this.saveViewData()
   }
 
-    // Saves this recipe's index to viewed recipe indexes in local storage
+  // Saves this recipe's index to viewed recipe indexes in local storage
   saveViewData = async () => {
     try {
       // Deletes an earlier view index if found
@@ -90,106 +90,111 @@ export default class RecipeInfoScreen extends React.Component {
       await AsyncStorage.setItem('viewedRecipeIndexes', JSON.stringify(this.state.viewedRecipeIndexes))
       console.log("saved to view history")
     } catch (error) {
-        console.log(error.mesage)
+      console.log(error.mesage)
     }
-}
+  }
 
   // Saves pin change to pinned recipe indexes in local storage
   saveChange = async () => {
     try {
-        await AsyncStorage.setItem('pinnedRecipeIndexes', JSON.stringify(this.state.pinnedRecipeIndexes));
+      await AsyncStorage.setItem('pinnedRecipeIndexes', JSON.stringify(this.state.pinnedRecipeIndexes));
     } catch (error) {
-        console.log(error.mesage)
+      console.log(error.mesage)
     }
-}
-
-// Flip-flops the pin state after change
-togglePin = () => {
-  this.setState({
-    isPinned: !this.state.isPinned
-  })
-  this.handlePin()
-}
-
-// This only goes through the if() once, for the sake of getting and setting this recipe's index
-setIndex(id) {
-  if (this.state.index == null) { // To prevent a loop of setting state and rendering
-    this.setState({
-      index: id
-    })
-    this.getViewData()
   }
-}
+
+  // Flip-flops the pin state after change
+  togglePin = () => {
+    this.setState({
+      isPinned: !this.state.isPinned
+    })
+    this.handlePin()
+  }
+
+  // This only goes through the if() once, for the sake of getting and setting this recipe's index
+  setIndex(id) {
+    if (this.state.index == null) { // To prevent a loop of setting state and rendering
+      this.setState({
+        index: id
+      })
+      this.getViewData()
+    }
+  }
 
 
-render() {
+  render() {
 
-  const { item: id } = this.props.route.params;
-  this.setIndex(id)
+    const { item: id } = this.props.route.params;
+    this.setIndex(id)
 
-  return (
+    return (
 
-   <SafeAreaView style={MealStyles.infoContainer}>
-     <ScrollView> 
-      <View style={MealStyles.infoImageContainer}>
-        <Image
-          style={MealStyles.infoImage}
-          source={{ uri: recipe[id].image_urls[0] }}
-        />
-      </View>
-        <View style={MealStyles.infoRecipeContainer}>
-        <Text style={MealStyles.infoTextTitle}>{recipe[id].title}</Text>
-        <Text style={MealStyles.infoTextSubtitle}>{recipe[id].subtitle}</Text>
-
-        <View style={MealStyles.infoHeadContainer}>
-          <Ionicons name={"time"} color={"black"} />
-          <Text style={MealStyles.infoTextHead}>{recipe[id].head[0]}</Text>
-          <Ionicons name={"hourglass"} color={"black"} />
-          <Text style={MealStyles.infoTextHead}>{recipe[id].head[1]}</Text>
-          <Ionicons name={"people"} color={"black"} />
-          <Text style={MealStyles.infoTextHead}>{recipe[id].head[2]}</Text>
-          <Ionicons name={"book"} color={"black"} />
-          <Text style={MealStyles.infoTextHead}>{recipe[id].head[3]}</Text>
-          
-          <Ionicons name={"pin"} color={"black"} />
-          <TouchableOpacity style={styles.toggleButton} onPress={this.togglePin}>
-          <Text style={MealStyles.infoTextHead}> {this.state.isPinned ? "Pinned" : "Pin"} </Text>
+      <SafeAreaView style={MealStyles.infoContainer}>
+        <View style={{ flexDirection: "row", paddingTop: '8%' }} >
+          <TouchableOpacity style={{ flex: 1 }} onPress={() => this.props.navigation.goBack()}>
+            <Ionicons name="arrow-back" size={25} />
           </TouchableOpacity>
-
         </View>
-      </View> 
+        <ScrollView>
+          <View style={MealStyles.infoImageContainer}>
+            <Image
+              style={MealStyles.infoImage}
+              source={{ uri: recipe[id].image_urls[0] }}
+            />
+          </View>
+          <View style={MealStyles.infoRecipeContainer}>
+            <Text style={MealStyles.infoTextTitle}>{recipe[id].title}</Text>
+            <Text style={MealStyles.infoTextSubtitle}>{recipe[id].subtitle}</Text>
 
-      <Text style={MealStyles.infoTextTitleBox}>ingredients</Text>
-      <View style={MealStyles.infoTitleContainer}>
-            
-      <FlatList
-            style={MealStyles.infoInstructionsContainer}
-            showsVerticalScrollIndicator={false}
-            keyboardDismissMode="on-drag"
-            data={recipe[id].ingredients}
-            renderItem={({ item }) => (
-              <Text style={MealStyles.infoIngredientsText}>{item}</Text>
-            )}
-          />
-      </View>
+            <View style={MealStyles.infoHeadContainer}>
+              <Ionicons name={"time"} color={"black"} />
+              <Text style={MealStyles.infoTextHead}>{recipe[id].head[0]}</Text>
+              <Ionicons name={"hourglass"} color={"black"} />
+              <Text style={MealStyles.infoTextHead}>{recipe[id].head[1]}</Text>
+              <Ionicons name={"people"} color={"black"} />
+              <Text style={MealStyles.infoTextHead}>{recipe[id].head[2]}</Text>
+              <Ionicons name={"book"} color={"black"} />
+              <Text style={MealStyles.infoTextHead}>{recipe[id].head[3]}</Text>
 
-      <Text style={MealStyles.infoTextTitleBox}>instructions</Text>
-      <View style={MealStyles.infoTitleContainer}>
+              <Ionicons name={"pin"} color={"black"} />
+              <TouchableOpacity style={styles.toggleButton} onPress={this.togglePin}>
+                <Text style={MealStyles.infoTextHead}> {this.state.isPinned ? "Pinned" : "Pin"} </Text>
+              </TouchableOpacity>
+
+            </View>
+          </View>
+
+          <Text style={MealStyles.infoTextTitleBox}>ingredients</Text>
+          <View style={MealStyles.infoTitleContainer}>
+
             <FlatList
-            style={MealStyles.infoInstructionsContainer}
-            showsVerticalScrollIndicator={false}
-            keyboardDismissMode="on-drag"
-            data={recipe[id].instructions}
-            renderItem={({ item }) => (
-              <Text style={MealStyles.infoInstructionsText}>{item}</Text>
-            )}
-          />
-      </View>
-      
-      </ScrollView> 
-    </SafeAreaView> 
-  ); 
-}
+              style={MealStyles.infoInstructionsContainer}
+              showsVerticalScrollIndicator={false}
+              keyboardDismissMode="on-drag"
+              data={recipe[id].ingredients}
+              renderItem={({ item }) => (
+                <Text style={MealStyles.infoIngredientsText}>{item}</Text>
+              )}
+            />
+          </View>
+
+          <Text style={MealStyles.infoTextTitleBox}>instructions</Text>
+          <View style={MealStyles.infoTitleContainer}>
+            <FlatList
+              style={MealStyles.infoInstructionsContainer}
+              showsVerticalScrollIndicator={false}
+              keyboardDismissMode="on-drag"
+              data={recipe[id].instructions}
+              renderItem={({ item }) => (
+                <Text style={MealStyles.infoInstructionsText}>{item}</Text>
+              )}
+            />
+          </View>
+
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
